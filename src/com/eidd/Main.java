019894187c2b4f47f6058c057e1de4e2bb533201
G-Ray <geoffrey.bonneville@gmail.com>
2015-01-10 20:33:52 +0100

    Ajout d'un compteur de temps d'éxécution

--- src/com/eidd/Main.java
+++ src/com/eidd/Main.java
@@ -5,13 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         int graph[][] = new int[][]{
-                {0, 1, 3, 4, 5},
-                {1, 0, 1, 4, 8},
-                {3, 1, 0, 5, 1},
-                {4, 4, 5, 0, 2},
-                {5, 8, 1, 2, 0}};
+                {0, 1, 3, 4, 5, 8},
+                {1, 0, 1, 4, 8, 8},
+                {3, 1, 0, 5, 1, 7},
+                {4, 4, 5, 0, 2, 3},
+                {5, 8, 1, 2, 0, 2},
+                {8, 8, 7, 3, 2, 0}};
 
         Backtracking b = new Backtracking(graph);
-        b.genererTousLesCircuits(0, 5);
+        long temps_debut = System.currentTimeMillis();
+        b.genererTousLesCircuits(0, graph.length);
+        System.out.println("Backtracking : " + (System.currentTimeMillis() - temps_debut) + "ms");
     }
 }
